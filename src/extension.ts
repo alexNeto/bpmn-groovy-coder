0eeed3c05ab0b63e2595be15446b4743979c5c91
alexNeto <58237299+alexNeto@users.noreply.github.com>
2022-03-08 00:32:47 -0300

    Add code needed for deploy bpmn implementation
    - Add input box for url
    - Add storage savice to help store the last input given

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext, window } from "vscode";
+import { Actions } from "./main/enum/actions.enum";
+import { StorageKeys } from "./main/enum/storage-keys.enum";
+import { LocalStorageService } from "./main/services/local-storage.service";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bpmn-coder" is now active!');
+export function activate(context: ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bpmn-coder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-	});
+  let disposable = commands.registerCommand(Actions.deploy, () => {
+    let localStorage = new LocalStorageService(context.workspaceState);
 
-	context.subscriptions.push(disposable);
+    window
+      .showInputBox({
+        placeHolder: "Camunda URL",
+        value: localStorage.getValue(StorageKeys.deploymentUrl),
+      })
+      .then(
+        (result) => {
+          localStorage.setValue(StorageKeys.deploymentUrl, result);
+          window.showInformationMessage(
+            "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+          );
+        },
+        (err) => {
+          window.showErrorMessage(
+            `AAAAAAA${err}AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA`
+          );
+        }
+      );
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/main/enum/storage-keys.enum.ts
@@ -0,0 +1,3 @@
+export enum StorageKeys {
+    deploymentUrl = "deployment-url"
+}
