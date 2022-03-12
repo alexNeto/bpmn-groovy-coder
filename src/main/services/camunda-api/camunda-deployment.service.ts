0f392a4ed1c90241ae3825b93b64219578b93c04
alexNeto <58237299+alexNeto@users.noreply.github.com>
2022-03-12 20:19:51 -0300

    remove linter warning from fromdata class

--- src/main/services/camunda-api/camunda-deployment.service.ts
+++ src/main/services/camunda-api/camunda-deployment.service.ts
@@ -3,13 +3,13 @@ import { MimeType } from "../../enum/mime-type.enum";
 import { HttpService } from "../http.service";
 import { CreateRequestBody } from "./interfaces/create/create-request-body.interface";
 
-const FormData = require("form-data");
+const FORM_DATA = require("form-data");
 
 export class CamundaDeploymentService {
   constructor(private httpService: HttpService) {}
 
   public create(url: string, createRequest: CreateRequestBody) {
-    const form = new FormData();
+    const form = new FORM_DATA();
 
     form.append("deployment-name", createRequest.deploymentName);
 
